9006fee8e3cb67233d541cfd7c9e2b3016bf70c4
chum-o <sammymichelsen@gmail.com>
2022-06-22 17:03:27 -0500

    dump

---
+++ wiki.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("wiki-container").style.marginLeft = "280px";
+  document.getElementById("wiki-container").style.width="75%"
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("wiki-container").style.marginLeft = "100px";
+  document.getElementById("wiki-container").style.width="90%"
+}
